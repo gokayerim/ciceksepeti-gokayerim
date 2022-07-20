2b511d1f3bb668d039b663a44b2786adca77d71d
Gokay Erim <gerim@ebay.com>
2022-07-20 14:17:35 +0300

    mobile tooltip

---
+++ src/components/CategorySelection/styles/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Button from "../../Button";
+
+export const StyledCategorySelectionWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: space-around;
+  margin-top: 1rem;
+`;
+
+export const StyledCatalogButton = styled(Button)`
+  width: 100px;
+`;
